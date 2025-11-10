9b8fab2251da4269990bd85b0dc282442176be54
byte-miner <harukin399@gmail.com>
2025-11-10 11:05:46 +0900

    fix build

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,8 +1,7 @@
 import { MetadataRoute } from 'next';
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = 'https://harukinakamura.dev'; // Replace with your actual domain
-
+  const baseUrl = 'https://harukinakamura.dev'; 
   return [
     {
       url: baseUrl,
